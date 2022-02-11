3532c9a77237a3ec0b05cdbae3f740289e5132dd
Håkan Sundström <sundarenius@gmail.com>
2022-02-11 20:28:53 +0100

    initloader

--- src/components/InitLoader.tsx
+++ src/components/InitLoader.tsx
@@ -3,15 +3,12 @@ import { Placeholder, Card } from 'semantic-ui-react'
 
 interface Props {}
 
-const InitLoader:FC<Props> = (): JSX.Element => {
-  const txt = 'Loader page ...';
-  return (
-    <Card id="placeholder-loader">
-      <Placeholder>
-        <Placeholder.Image square />
-      </Placeholder>
-    </Card>
-  );
-};
+const InitLoader:FC<Props> = (): JSX.Element => (
+  <Card id="placeholder-loader">
+    <Placeholder>
+      <Placeholder.Image square />
+    </Placeholder>
+  </Card>
+);
 
 export default InitLoader;
